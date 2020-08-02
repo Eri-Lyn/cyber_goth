c875009032c3626254cf0083833f1298e6e8250d
Eri <elindstrlu@gmail.com>
2020-08-02 17:24:13 -0400

    Added prompt to assignment_2

---
+++ andrew_assignment_2.py
@@ -0,0 +1,15 @@
+"""
+ASSIGNMENT 2 -- CLASSES
+````````````````````````````````````````````````````````````````````````````````````````
+1. Create a class called 'Animal'. It should have the following member variables:
+   name  (ex. Cow)
+   color (ex. Brown)
+   sound (ex. Moo)
+   type  (ex. Mammal)
+
+2. Make a LIST of this Animal type. This list should be iterable and callable. In other words, if you return
+   animalList[2], it should neatly output those member variables in some fashion. Likewise, you need a way
+   to sort this class (which I will leave up to you). Attempting to call sort(animalList) should return a list
+   of Animal objects organized by X member variable.
+
+"""
