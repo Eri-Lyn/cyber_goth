db97c6b97683936b2c8798bcfd388884f9ca2a25
aschofield18 <a.schofie14@gmail.com>
2020-09-02 17:02:54 -0400

    Update andrew_assignment_2.py

--- andrew_assignment_2.py
+++ andrew_assignment_2.py
@@ -13,3 +13,60 @@ ASSIGNMENT 2 -- CLASSES
    of Animal objects organized by X member variable.
 
 """
+
+from operator import attrgetter
+
+class Animal:
+   def __init__ (self,name,color,sound,type):
+      self.name = name
+      self.color = color
+      self.sound = sound
+      self.type = type
+      
+   def __str__(self):
+      return f"{self.name} is a {self.color} color"
+   
+   def speak(self, sound):
+      return f"{self.name} says {sound}"
+
+cow = Animal("cow","brown","moo","mammal")
+dog = Animal("dog","black","bark","mammal")
+rooster = Animal("rooster","red","crow","reptile")
+horse = Animal("horse","tan","neigh","mammal")
+cat = Animal("cat","white","meow","mammal")
+bird = Animal("bird","red","chirp","reptile")
+cat1 = Animal("cat","black","meow","mammal")
+
+#animalList = ['cow','dog','rooster','horse','cat']
+
+
+animalList = []
+# animalList.append(Animal("cow","brown","moo","mammal"))
+# animalList.append(Animal("dog","black","bark","mammal"))
+# animalList.append(Animal("rooster","red","crow","reptile"))
+# animalList.append(Animal("horse","tan","neigh","mammal"))
+# animalList.append(Animal("cat","white","meow","mammal"))
+# animalList.append(Animal("cat","black","meow","mammal"))
+
+animalList.append(bird)
+animalList.append(cow)
+animalList.append(dog)
+animalList.append(rooster)
+animalList.append(horse)
+animalList.append(cat)
+animalList.append(cat1)
+
+print("Sort by...")
+animalList.sort(key=attrgetter('name'))
+
+print("...then show all animals and color:")
+for animal in animalList:
+   print("%s: \"%s\"" % (animal.name, animal.color))
+
+look = input('What animal do you want? ')
+for animal in animalList:
+   if look in animal.name:
+      #print("%s: \"%s\" \"%s\" \"%s\"" % (animal.name, animal.color, animal.sound, animal.type))
+      print(animal.name + ' says ' + animal.sound)
+
+
